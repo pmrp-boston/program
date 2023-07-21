e5cf11fc19c983ee5b26c95c91768d662c5075a6
Sidney Hackney <snackney@gmail.com>
2023-07-20 21:14:37 -0400

    Include additions from scarlet - audition footer and beginning of theme styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import CreditsBlock from "../components/CreditsBlock.js";
 import Bios from "../components/Bios.js";
 import ResponsiveHeroImage from "../components/ResponsiveHeroImage.js";
+import AuditionFooter from "../components/AuditionFooter.js";
 import {
   scroller,
 } from "react-scroll";
@@ -16,15 +17,6 @@ const IndexPage = () => {
       <header>
         <h4 className="preHeader">The Post Meridian Radio Players Present</h4>
         <ResponsiveHeroImage />
-        {useWindowDimensions().width < 530 && (
-          <h4 className="postHeader">
-            Plus! The never before revealed to the public{" "}
-            <span className="highlightText">
-              "Plan 8 from the Outer Ether!"
-            </span>{" "}
-            by Brian Rust
-          </h4>
-        )}
       </header>
       <div className="content">
         <div className="credits">
@@ -38,7 +30,7 @@ const IndexPage = () => {
         </div>
       </div>
 
-      <footer>{/*<SocialMediaLinks />*/}</footer>
+      <footer><AuditionFooter /></footer>
     </body>
   );
 };
