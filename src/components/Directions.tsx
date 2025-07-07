723dc9ecba227472442e36600d59eff5bee9032d
Sidney Hackney <hacksid.official@gmail.com>
2025-07-07 19:52:04 -0400

    Add individual show json files, stub directions and info components, add logo files

---
+++ src/components/Directions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Directions = () => {
+  return (
+    <div>
+      <h2>Directions</h2>
+      <p>Coming Soon!</p>
+    </div>
+  );
+};
+
+export default Directions;
---
+++ src/components/Information.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Information = () => {
+  return (
+    <div>
+      <h2>Information and Accessibility</h2>
+      <p>Coming Soon!</p>
+    </div>
+  );
+};
+
+export default Information;
